e048bce48233cb917a4dd6c5cc422f9be8d95002
fusae <fusae_js@163.com>
2016-08-31 00:22:05 +0800

    third

---
+++ iconv.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+
+def iconv(path):
+    command = 'iconv -f cp936 -t utf-8 {}'.format(path)
+
+    res = subprocess.check_output(command, shell=True)
+    res = res.decode('utf-8')
+#    print(res)
+#    print('--------------------------------------------------')
+
+    with open(path, 'w') as f:
+        f.write(res)
+
+        f.close()
+
+def traverse(path):
+    names = os.listdir(path)
+    for name in names:
+        name = path + '/' + name
+        if os.path.isdir(name):
+            traverse(name)
+#        print(name)
+        if ('.txt' in name):
+            print(name)
+            iconv(name)
+
+if __name__ == '__main__':
+    path = 'HTML与CSS'
+
+    traverse(path)
+
